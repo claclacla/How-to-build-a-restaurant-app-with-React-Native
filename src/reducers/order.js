ff64183215e7a7016a6963c674d421d44b24278f
Adelchino Simone <seconddesire@gmail.com>
2018-10-30 22:13:12 +0100

    If a dish added to the order is already present, the previous entry is overwritten

--- src/reducers/order.js
+++ src/reducers/order.js
@@ -2,21 +2,34 @@ import { ADD_PRODUCT, UPDATE_PRODUCT_AMOUNT, REMOVE_PRODUCT } from '../actions/o
 
 import Order from '../entities/Order';
 
-function order(state = new Order(), action) { 
+function order(state = new Order(), action) {
   switch (action.type) {
-    case ADD_PRODUCT:           
+    case ADD_PRODUCT:
+      let productIdx = state.products.findIndex(product => product.uid === action.product.uid);
+
+      let products = [
+        ...state.products
+      ];
+
+      if(productIdx >= 0) {
+        products[productIdx] = action.product;
+      }
+      else {
+        products = products.concat(action.product);
+      }
+
       return {
         ...state,
-        products: state.products.concat(action.product)
+        products
       };
-    case UPDATE_PRODUCT_AMOUNT:     
+    case UPDATE_PRODUCT_AMOUNT:
       return {
         ...state,
         products: state.products.map(product => {
-          if(product.uid === action.productUid) {
+          if (product.uid === action.productUid) {
             product.amount = action.amount;
           }
-  
+
           return product;
         })
       };
@@ -24,13 +37,13 @@ function order(state = new Order(), action) {
       return {
         ...state,
         products: state.products.filter(product => {
-          if(product.uid !== action.productUid) {
+          if (product.uid !== action.productUid) {
             return true;
           }
 
           return false;
         })
-      }  
+      }
     default:
       return state;
   }
